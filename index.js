36de5c0b0d6496752b1bb9a64a3d9583dd78860c
Isaque-Esdras <exploreapps92@gmail.com>
2022-11-16 17:50:14 -0300

    a commit

---
+++ index.js
@@ -0,0 +1,106 @@
+const inpute_cep = document.getElementById('input-cep')
+const butt_cep = document.getElementById('but-input')
+const h_logradouro = document.getElementById('logradouro')
+const h_bairro = document.getElementById('bairro')
+const h_cidade = document.getElementById('cidade')
+const h_estado = document.getElementById('estado')
+
+butt_cep.addEventListener('click', () =>{
+    let cep = inpute_cep.value
+
+    if (cep.length !== 8) {
+        alert('Digite a quantidade de números suficiente ou remova o dígito (-).')
+    }
+
+    fetch(`https://viacep.com.br/ws/${cep}/json/`)
+    .then(resposta => resposta.json())
+    .then(json=>{
+
+        h_logradouro.innerHTML = 'Logradouro: ' + json.logradouro 
+        h_bairro.innerHTML = 'Bairro: ' + json.bairro 
+        h_cidade.innerHTML = 'Cidade: ' + json.localidade
+        h_estado.innerHTML = 'UF: ' + json.uf
+    })
+    
+} )
+
+
+let infoCordenadas = document.getElementById('longlati')
+
+if (confirm('Você autoriza nosso site a ver sua localização e mostrá-la a você no mapa ?')) {
+    function erro(error) {
+        console.log(error)
+    }
+    
+    function sucesso(position) {
+      
+       infoCordenadas.innerHTML = `Latitude: ${position.coords.latitude} <br> Longitude: ${position.coords.longitude}`
+    
+       var map = L.map('map').setView([position.coords.latitude, position.coords.longitude], 13);
+    
+       L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+    
+       L.marker([position.coords.latitude,  position.coords.longitude]).addTo(map)
+        .bindPopup('Sua Localização')
+        .openPopup();
+    }
+    
+    
+    var posicaoUsuario = navigator.geolocation.watchPosition(sucesso, erro,{
+    
+        enableHighAccuracy:true,
+        timeout: 6000
+        
+    })
+}else{alert('Certo, sua Localização não será coletada.')}
+
+
+
+
+//navigator.geolocation.clearWatch(posicaoUsuario)
+
+
+gerarQr =()=>{
+    var inpute = document.getElementById('input-qr').value
+    var apiQrCode = `https://chart.googleapis.com/chart?cht=qr&chs=300x300&chl=${inpute}`
+    document.querySelector('#qr-code-image').src = apiQrCode
+}
+
+
+pagQr = () =>{
+    window.location.href = '#section-qr-central'
+}
+
+lgpd = () =>{
+    window.location.href = './Politica de Privacidade/lgpd.html'
+}
+
+butAmazonia = () =>{
+    window.location.href = '#article-amazonia'
+}
+
+videoUm = () =>{
+    window.location.href = 'https://www.youtube.com/watch?v=X3gvWKyZy_s'
+}
+
+videoDois = () =>{
+    window.location.href = 'https://www.youtube.com/watch?v=OODsSZ5KDnI'
+}
+
+videoTres = () =>{
+    window.location.href = 'https://www.youtube.com/watch?v=XpLoUmn4yG8'
+}
+
+videoQuatro = () =>{
+    window.location.href = 'https://www.youtube.com/watch?v=uQVOMoTNPAI'
+}
+
+videoCinco = () =>{
+    window.location.href = 'https://www.youtube.com/watch?v=XK2uVOiRQv8'
+}
+
+videoSeis = () =>{
+    window.location.href = 'https://www.youtube.com/watch?v=1RkUDzYeGjw'
+}
